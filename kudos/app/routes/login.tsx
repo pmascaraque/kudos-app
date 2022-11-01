678b72567901703f16de9a95babd63d47a078790
Pablo Moreno <ssm.pablo@gmail.com>
2022-11-02 00:50:08 +0100

    form field modularized

--- kudos/app/routes/login.tsx
+++ kudos/app/routes/login.tsx
@@ -1,6 +1,23 @@
+import { useState } from "react";
+import { FormField } from "~/components/form-field";
 import { Layout } from "~/components/layout";
 
 export default function Login() {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleInputChange = (
+    event: React.ChangeEvent<HTMLInputElement>,
+    field: string
+  ) => {
+    setFormData((form) => ({
+      ...form,
+      [field]: event.target.value,
+    }));
+  };
+
   return (
     <Layout>
       <div className='h-full flex justify-center items-center flex-col gap-y-4'>
@@ -11,35 +28,19 @@ export default function Login() {
           Log In To Give Some Praise!
         </p>
 
-        <form method='post' className='rounded-2xl bg-gray-200 p-6 w-96'>
-          <label htmlFor='email' className='text-blue-600 font-semibold'>
-            Email
-          </label>
-          <input
-            type='text'
-            id='email'
-            name='email'
-            className='w-full p-2 rounded-xl my-2'
-          />
-
-          <label htmlFor='password' className='text-blue-600 font-semibold'>
-            Password
-          </label>
-          <input
-            type='password'
-            id='password'
-            name='password'
-            className='w-full p-2 rounded-xl my-2'
-          />
-
-          <div className='w-full text-center'>
-            <input
-              type='submit'
-              className='rounded-xl mt-2 bg-yellow-300 px-3 py-2 text-blue-600 font-semibold transition duration-300 ease-in-out hover:bg-yellow-400 hover:-translate-y-1'
-              value='Sign In'
-            />
-          </div>
-        </form>
+        <FormField
+          htmlFor='email'
+          label='Email'
+          value={formData.email}
+          onChange={(e) => handleInputChange(e, "email")}
+        />
+        <FormField
+          htmlFor='password'
+          label='Password'
+          value={formData.password}
+          onChange={(e) => handleInputChange(e, "password")}
+          type='password'
+        />
       </div>
     </Layout>
   );
