7398f0f2520bde846dcc23548035c85803e71afe
Pablo Moreno <ssm.pablo@gmail.com>
2022-11-02 00:32:30 +0100

    login page route

---
+++ kudos/app/routes/login.tsx
@@ -0,0 +1,9 @@
+import { Layout } from "~/components/layout";
+
+export default function Login() {
+  return (
+    <Layout>
+      <h2 className='font-bold text-5xl text-blue-400'>Login</h2>
+    </Layout>
+  );
+}
