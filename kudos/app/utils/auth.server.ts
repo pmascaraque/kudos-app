cfaeba1ac20de9c37b4cbf606e14bbd5e78507d4
Pablo Moreno <ssm.pablo@gmail.com>
2022-11-02 20:16:41 +0100

    registration handling

---
+++ kudos/app/utils/auth.server.ts
@@ -0,0 +1,29 @@
+import { json } from "@remix-run/node";
+import { prisma } from "./prisma.server";
+import { RegisterForm } from "./types.server";
+import { createUser } from "./users.server";
+
+export const register = async (form: RegisterForm) => {
+  const exists = await prisma.user.count({ where: { email: form.email } });
+
+  if (exists) {
+    return json(
+      { error: `User already exists with that email` },
+      { status: 400 }
+    );
+  }
+
+  const newUser = await createUser(form)
+
+  if (!newUser) {
+    return json(
+      {
+        error: `Something went wrong trying to create a new user`,
+        fields: { email: form.email, password: form.password }
+      },
+      { status: 400 }
+    )
+  }
+
+  return null;
+}
