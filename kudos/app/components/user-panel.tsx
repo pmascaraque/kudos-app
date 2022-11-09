b8cd11efcdc8e6f624315105a242f834464225cf
Pablo Moreno <ssm.pablo@gmail.com>
2022-11-09 23:43:09 +0100

    finished team panel

--- kudos/app/components/user-panel.tsx
+++ kudos/app/components/user-panel.tsx
@@ -1,11 +1,23 @@
-export function UserPanel() {
+import { User } from "@prisma/client";
+import { UserCircle } from "./user-circle";
+interface props {
+  users: User[];
+}
+
+export function UserPanel({ users }: props) {
   return (
     <div className='w-1/6 bg-gray-200 flex flex-col'>
       <div className='text-center bg-gray-300 h-20 flex items-center justify-center'>
         <h2 className='text-xl text-blue-600 font-semibold'>My Team</h2>
       </div>
       <div className='flex-1 overflow-y-scroll py-4 flex flex-col gap-y-10'>
-        <p>Users go here</p>
+        {users.map((user) => (
+          <UserCircle
+            profile={user.profile}
+            key={user.id}
+            className='h-24 w-24 mx-auto flex-shrink-0'
+          />
+        ))}
       </div>
       <div className='text-center p-6 bg-gray-300'>
         <button
--- kudos/app/routes/home.tsx
+++ kudos/app/routes/home.tsx
@@ -1,13 +1,23 @@
-import { LoaderFunction } from "@remix-run/node";
+import { json, LoaderFunction } from "@remix-run/node";
 import { Layout } from "~/components/layout";
 import { UserPanel } from "~/components/user-panel";
 import { requireUserId } from "~/utils/auth.server";
+import { GetOtherUsers } from "~/utils/users.server";
+import { useLoaderData } from "@remix-run/react";
 
 export const loader: LoaderFunction = async ({ request }) => {
-  await requireUserId(request);
-  return null;
+  const userId = await requireUserId(request);
+  const users = await GetOtherUsers(userId);
+  return json({ users });
 };
 
 export default function Home() {
-  return <Layout><div className="h-full flex"><UserPanel /></div></Layout>;
+  const { users } = useLoaderData();
+  return (
+    <Layout>
+      <div className='h-full flex'>
+        <UserPanel users={users} />
+      </div>
+    </Layout>
+  );
 }
