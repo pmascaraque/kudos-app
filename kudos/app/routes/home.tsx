205b275b0d345c7b85f09c7e635bc2f6bb4d4980
Pablo Moreno <ssm.pablo@gmail.com>
2022-12-03 01:43:36 +0100

    add user kudo modal

--- kudos/app/routes/home.tsx
+++ kudos/app/routes/home.tsx
@@ -1,9 +1,10 @@
-import { json, LoaderFunction } from "@remix-run/node";
+import type { LoaderFunction } from "@remix-run/node";
+import { json } from "@remix-run/node";
 import { Layout } from "~/components/layout";
 import { UserPanel } from "~/components/user-panel";
 import { requireUserId } from "~/utils/auth.server";
 import { GetOtherUsers } from "~/utils/users.server";
-import { useLoaderData } from "@remix-run/react";
+import { useLoaderData, Outlet } from "@remix-run/react";
 
 export const loader: LoaderFunction = async ({ request }) => {
   const userId = await requireUserId(request);
@@ -15,6 +16,7 @@ export default function Home() {
   const { users } = useLoaderData();
   return (
     <Layout>
+      <Outlet />
       <div className='h-full flex'>
         <UserPanel users={users} />
       </div>
---
+++ kudos/app/routes/home/kudo.$userId.tsx
@@ -0,0 +1,9 @@
+import { Modal } from "~/components/modal";
+
+export default function KudoModal() {
+  return (
+    <Modal isOpen={true} className='w-2/3 p-10'>
+      <h2> User Modal Modal Modal</h2>
+    </Modal>
+  );
+}
